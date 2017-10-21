3e8fbd415c9b39809095684c7c8aa7845c8c6bb1
Gabriel Klockner <gabrielklockner@Gabriels-MacBook-Pro.local>
2017-10-21 01:33:37 -0200

    Add Devise authentication for User

---
+++ db/migrate/20171021024101_add_columns_to_users.rb
@@ -0,0 +1,23 @@
+class AddColumnsToUsers < ActiveRecord::Migration[5.1]
+  def up
+    add_column :users, :name, :string
+    change_column_null :users, :name, false
+
+    add_column :users, :username, :string, index: true, unique: true
+    change_column_null :users, :username, false
+
+    add_column :users, :cpf, :string, index: true, unique: true
+    add_column :users, :birth, :date
+    add_column :users, :cellphone, :string
+    add_column :users, :country, :string
+    add_column :users, :state, :string
+    add_column :users, :city, :string
+    add_column :users, :zipcode, :string
+    add_column :users, :address, :string
+  end
+
+  def down
+    remove_column :users, :name, :string
+    remove_column :users, :username, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171019015742) do
+ActiveRecord::Schema.define(version: 20171021024101) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20171019015742) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "username", null: false
+    t.string "cpf"
+    t.date "birth"
+    t.string "cellphone"
+    t.string "country"
+    t.string "state"
+    t.string "city"
+    t.string "zipcode"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
